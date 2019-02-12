e0192f477b2f8ef1331bb8757d40ea6dc51064b0
iait <46071420+iait@users.noreply.github.com>
2019-02-11 21:10:02 -0300

    primera versión para subir y bajar archivos desde el file system

--- src/main/java/iait/eiv/Application.java
+++ src/main/java/iait/eiv/Application.java
@@ -2,8 +2,10 @@ package iait.eiv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({FileStorageProperties.class})
 public class Application {
 
     public static void main(String[] args) {
